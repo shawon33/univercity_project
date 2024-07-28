a083d0cb778cc9a9db40ad5b7881fb0cf6eaab57
shawon33 <iamcoming115@gmail.com>
2024-07-28 08:40:04 +0600

    add custore base api

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -5,18 +5,23 @@ import { RootState } from '../features/store'
 const baseQuery = fetchBaseQuery({
     baseUrl: 'http://localhost:5000/api/v1',
     credentials: 'include',
-    prepareHeaders:(headers,{getState})=>{
-        const token  = (getState() as RootState).auth.token;
+    prepareHeaders: (headers, { getState }) => {
+        const token = (getState() as RootState).auth.token;
 
-        if(token) {
-            headers.set('authorization',`${token}`)
+        if (token) {
+            headers.set('authorization', `${token}`)
         }
         return headers
     },
 })
 
+const baseQueryWithFreshToken = async (args, api, extraOptions) => {
+    const result = await baseQuery(args, api, extraOptions);
+    console.log(result);
+}
+
 export const baseApi = createApi({
     reducerPath: 'baseApi',
-    baseQuery: baseQuery ,
+    baseQuery: baseQueryWithFreshToken,
     endpoints: () => ({})
 }) 
